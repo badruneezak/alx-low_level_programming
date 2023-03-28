b4d59db959abab292ba96c89fdc6e67540c51713
Badruneeza <badruneezak@gmail.com>
2023-03-28 13:46:22 -0700

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,7 +5,9 @@
 * @n: input
 * Return: n
 */
+
 void reset_to_98(int *n)
 {
+
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,22 @@
-add:wq:
+#include "main.h"
+/**
+* print_array -  a function that prints n elements of an array of integers, followed by a new line
+* @a:
+* @b:
+* Return:
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, " , a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d" , a[n - 1]);
+	}
+		printf("\n");
+}
