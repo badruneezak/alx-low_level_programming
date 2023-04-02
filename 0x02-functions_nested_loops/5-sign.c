48cf1e423b2b6f34e8326b7032ee29471bc2c5bc
Badruneeza <badruneezak@gmail.com>
2023-04-02 08:27:38 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,27 +5,13 @@
 * Return: returns 1 for positive number
 * and 0 otherwise
 */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
+		return (1);
 	}
 	else if (n == 0)
-	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
-	}
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,22 +3,23 @@
 * jack_bauer - prints 24hrs time
 */
 
-void jack_bauer(void);
+void jack_bauer(void)
 {
-	int i , j;
-	{
+	int i, j;
 
 	i = 0;
+
 	while (i < 24)
+	{
 	
 		j = 0;
 		while (j < 60)
 		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
 		}
